45183ede17b4c074f732863a5ce44da59b39448b
EsteMaeLeo <leonel_morales20@hotmail.com>
2022-05-08 17:19:19 -0500

    comment all html after testing and refactor with correct class and adding javascript file

---
+++ assets/js/script.js
@@ -0,0 +1,28 @@
+var loadRowTimes = function () {
+  var currentHour = moment().format("HH");
+  //loop each row and class time block
+  $(".time-block").each(function (index, value) {
+    //get Id to know with hour and compare to add class to put colors
+
+    var id = $(this).attr("id");
+    const hour = id.replace("time-", "");
+    var intHour = parseInt(hour);
+    var intCurrent = parseInt(currentHour);
+
+    console.log(hour);
+
+    if (intHour < intCurrent) {
+      $(this).addClass("past");
+    } else if (intHour === intCurrent) {
+      $(this).addClass("present");
+    } else if (intHour > intCurrent) {
+      $(this).addClass("future");
+    }
+  });
+};
+
+var saveTasks = function () {
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+};
+
+loadRowTimes();
