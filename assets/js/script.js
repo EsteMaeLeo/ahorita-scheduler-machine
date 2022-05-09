a52a323d199c88d83022fc2067528c965b08409e
EsteMaeLeo <leonel_morales20@hotmail.com>
2022-05-08 21:12:39 -0500

    refactor some part js code and adding to safe in localstorage validate if exist note and update or add new one

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,15 @@
+sheduleNote = [];
+
+sheduleStr = {};
+
 var loadRowTimes = function () {
   //display current date
   var CurrentDate = moment().format("MMM Do YYYY");
   $("#currentDay").append('<span id="add_here">' + CurrentDate + "</span>");
 
   var currentHour = moment().format("HH");
+  //load notes on the shedule
+  var localNotes = JSON.parse(localStorage.getItem("sheduleNote"));
   //loop each row and class time block
   $(".time-block").each(function (index, value) {
     //get Id to know with hour and compare to add class to put colors
@@ -18,6 +24,21 @@ var loadRowTimes = function () {
     var intCurrent = parseInt(currentHour);
     //intCurrent = 12;
 
+    // if localstorage loop validate with id get text
+    var time = "#" + id;
+    if (localNotes) {
+      for (var i = 0; i < localNotes.length; i++) {
+        if (localNotes[i].id === id) {
+          var valNote = localNotes[i].note;
+          break;
+        }
+      }
+    }
+    if (valNote) {
+      //set the text
+      $(time + " .note").val(valNote);
+    }
+
     //validate to set colour past, present and future
     if (intHour < intCurrent) {
       $(this).addClass("past");
@@ -29,8 +50,51 @@ var loadRowTimes = function () {
   });
 };
 
-var saveTasks = function () {
-  localStorage.setItem("tasks", JSON.stringify(tasks));
+$(".saveBtn").click(function () {
+  //click save button get the id of the row
+  var id = $(this).parent().attr("id");
+  //get the text
+  var noteText = $(this).siblings(".note").val();
+
+  if (noteText) {
+    // save in tasks structure
+    sheduleStr.id = id;
+    sheduleStr.note = noteText;
+
+    saveNotes();
+  }
+});
+
+var saveNotes = function () {
+  //variable if found record in case update note shedule
+  var found = false;
+  //get old notes
+  var oldNotes = JSON.parse(localStorage.getItem("sheduleNote"));
+  if (oldNotes) {
+    for (var i = 0; i < oldNotes.length; i++) {
+      if (oldNotes[i].id === sheduleStr.id) {
+        //if get the note set new text note
+        oldNotes[i].note = sheduleStr.note;
+        localStorage.setItem("sheduleNote", JSON.stringify(oldNotes));
+        sheduleNote = {};
+        found = true;
+        break;
+      }
+    }
+  } else {
+    // there is not found old notes means new record
+    sheduleNote.push(sheduleStr);
+    localStorage.setItem("sheduleNote", JSON.stringify(sheduleNote));
+    sheduleNote = [];
+    return;
+  }
+
+  if (!found) {
+    //case not found but we have notes then safe the new
+    oldNotes.push(sheduleStr);
+    localStorage.setItem("sheduleNote", JSON.stringify(oldNotes));
+    sheduleNote = [];
+  }
 };
 
 loadRowTimes();
