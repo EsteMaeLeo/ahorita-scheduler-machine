0d56227957935cc23e0f53218525e4ccc79e51e7
EsteMaeLeo <leonel_morales20@hotmail.com>
2022-05-08 20:44:40 -0500

    refactor HTML to new rows for better display and use DOM with jquery

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,13 +8,17 @@ var loadRowTimes = function () {
   $(".time-block").each(function (index, value) {
     //get Id to know with hour and compare to add class to put colors
 
+    // get id attribut
     var id = $(this).attr("id");
+    //replace time- to get time
     const hour = id.replace("time-", "");
+    //parse to integer hour
     var intHour = parseInt(hour);
+    //get the current hour
     var intCurrent = parseInt(currentHour);
+    //intCurrent = 12;
 
-    console.log(hour);
-
+    //validate to set colour past, present and future
     if (intHour < intCurrent) {
       $(this).addClass("past");
     } else if (intHour === intCurrent) {
